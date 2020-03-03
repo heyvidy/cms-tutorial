12e7c721a67f349ca16618dc9c17d7559ef5ef4f
Vidyadhar <justvidyadhar@gmail.com>
2020-03-03 22:16:11 +0530

    Add authentication to blog app

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from blog.models import BlogPost
 from django.utils import timezone
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 
 def home(request):
@@ -9,6 +11,7 @@ def home(request):
 
 
 def blog_catalog(request):
+    print("Current User:", request.user)
     all_posts = BlogPost.objects.all()
     return render(request, "all_posts.html", {"all_posts": all_posts})
 
@@ -24,11 +27,55 @@ def create_post(request):
         content = request.POST.get('content')
         author = request.POST.get('author')
 
-        blog = BlogPost.objects.create(title=title, 
-                                        content=content, 
-                                        author=author, 
-                                        timestamp=timezone.now())
+        blog = BlogPost.objects.create(title=title,
+                                       content=content,
+                                       author=author,
+                                       timestamp=timezone.now())
 
         return redirect(f"/blog/{blog.id}/")
 
     return render(request, "create_post.html")
+
+
+def signup_page(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        fname = request.POST["fname"]
+        lname = request.POST["lname"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+
+        user = User.objects.create_user(
+            first_name=fname,
+            last_name=lname,
+            email=email,
+            username=username,
+            password=password,
+        )
+        print(user)
+
+        return redirect(f"/{user.username}")
+
+    return render(request, "signup.html")
+
+
+def signin(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("/")
+
+        else:
+            return HttpResponse("Invalid Credentials. <a href='/login'>Login again here</a>")
+
+    return render(request, "signin.html")
+
+
+def signout(request):
+    logout(request)
+    return redirect("/")
--- cms/urls.py
+++ cms/urls.py
@@ -1,18 +1,3 @@
-"""cms URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from blog.views import *
@@ -21,5 +6,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog_catalog),
     path('blog/<int:post_id>/', post_page),
-    path('create/', create_post)
+    path('create/', create_post),
+    path('signup/', signup_page),
+    path('login/', signin),
+    path('logout/', signout)
 ]
